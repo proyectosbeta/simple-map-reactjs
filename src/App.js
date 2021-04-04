dcf5f3d358f5babab296b84bcd6039afbacf469c
josego <josego85@gmail.com>
2021-04-04 08:34:00 -0400

    A simple map using Leaflet with GeoJSON in ReactJS.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Map from './components/Map';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Map/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import data from "../data/departament.json";
+
+const mapPosition = [-24.000000, -57.623807];
+const zoom = 7;
+const style = {
+  fillColor: "red",
+  fillOpacity: 1,
+  color: "black",
+  weight: 2,
+};
+
+const onEachFeature = (feature, layer) => {
+  let popupContent = `Departamento nombre: ${feature.properties.departamento_nombre} 
+    <br> Departamento capital: ${feature.properties.departamento_capital}`;
+  if (feature.properties && feature.properties.popupContent) {
+    popupContent += feature.properties.popupContent;
+  }
+  layer.bindPopup(popupContent);
+};
+
+function Map() {
+  return (
+    <div className="map-container">
+      <MapContainer
+        center={mapPosition}
+        zoom={zoom}
+        scrollWheelZoom={false}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoJSON
+          style={style}
+          data={data.features}
+          onEachFeature={onEachFeature}
+        /> 
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
